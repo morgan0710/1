5ab4c93b4bc8d42130ba65316c29c9e4e6c13b88
Ibragimov Jasurbek <morgan7122010@gmail.com>
2026-08-08 13:47:58 +0500

    Delete orm_app directory

--- orm_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_employees, get_jobs, get_countries
-
-urlpatterns = [
-    path('get_employees/', get_employees),
-    path('get_jobs/', get_jobs),
-    path('get_countries/', get_countries)
-]
